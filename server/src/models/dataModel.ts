c7a44a515021e5a674602d2b11308a8855241ba1
Hassan Sani <saniyhassan@gmail.com>
2019-03-25 12:28:15 +0100

    Added recusive types

--- server/src/models/dataModel.ts
+++ server/src/models/dataModel.ts
@@ -1,7 +1,46 @@
 import mongoose from 'mongoose';
 
 export type dataModel = mongoose.Document & {
-    
+    id: string,
+    age: number,
+    name: string,
+    phone: string,
+    geoInfo: {
+        latitude: number,
+        longitude: number
+    },
+    childrens: childrensModel
 }
 
-const dataSchema = new mongoose.Schema({})
+export type childrens = mongoose.Document & {
+    (x: object): dataModel,
+    connectionInfo: {
+        type: string,
+        confidence: number
+    }
+}
+
+interface childrensModel extends Array<childrens> {}
+
+const data = new mongoose.Schema({
+    id: String,
+    age: Number,
+    name: String,
+    phone: String,
+    geoInfo: {
+        latitude: Number,
+        longitude: Number
+    }
+});
+
+const childrens = data.clone();
+childrens.add({
+    connectionInfo: {
+        type: {type: String},
+        confidence: Number
+    }
+});
+
+data.add({
+    childrens: [childrens]
+});
